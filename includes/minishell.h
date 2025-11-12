b69441ad80ad5635c11ae85fcccd438fff6c0d84
AdnilsonPinheiro <adpmaia@gmail.com>
2025-11-12 19:40:59 +0000

    correct headers. Prototype static functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 16:36:50 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/11/12 18:19:20 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/11/12 19:30:53 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,50 +22,7 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdbool.h>
-# include "parser.h"
-
-
-typedef	enum e_redir_type
-{
-	REDIR_IN, // <
-	REDIR_OUT, // >
-	REDIR_APPEND, // >>
-	REDIR_HERE // <<
-}	e_redir_type;
-
-/// @brief these structs will form our environment.
-typedef	struct s_env
-{
-	char			*name; //variable's name.
-	char			*value; //variable's expansion.
-	struct s_env	*next;
-}	t_env;
-
-/// @brief redirection details. t_cmd will have a pointer to this.
-typedef	struct s_redir
-{
-	e_redir_type	type; //macros for the type of redirection (e.g. '<', '>>')
-	char			*file; //the name of the redirection file. If HEREDOC, this means EOF.
-	int				fd; //holds the redirection file's fd. Will be initialized to -1
-	struct s_redir	*next; //list of redirections (e.g. "< infile cat > outfile", which has both REDIR_IN and REDIR_OUT)
-}	t_redir;
-
-/// @brief main struct to be passed for execution
-typedef	struct s_cmd
-{
-	char			*cmd; //command name
-	char			**args; //arguments to the command
-	t_redir			*redirs; //linked list of redirections, NULL if none
-	struct s_cmd	*next; //if there is a pipe, the address of the next cmd will be here. If no, this will be NULL
-}	t_cmd;
-
-typedef	struct s_shell
-{
-	t_env	*env; //envp list
-	t_token	*tokens; //token list
-	t_cmd	*cmds; //command list
-	int		exit_status; //last command exit status for $?
-}	t_shell;
+# include "structs.h"
 
 /*****************************************************************************\
 *                                Parsing Functions                            *
@@ -75,13 +32,29 @@ void	loop(t_shell *shell);
 void	ft_init_shell(t_shell *shell, char **envp);
 void	ft_exit(t_shell *shell, char *input);
 void	ft_parser(t_shell *shell);
+void	ft_lexer(t_shell *shell, char *input);
+void	ft_add_token(t_token **lst, t_token *new);
+void	ft_settokentype(t_token *token);
+
+t_token	*ft_new_token(char *input, ssize_t size);
+t_token	*ft_init_token(ssize_t token_size);
+
+ssize_t	ft_tokensize(char *input);
+ssize_t	ft_operatorsize(char *input);
+
+bool	ft_isspace(const char c);
+bool	is_operator(const char *str);
+bool	is_quote(const char str);
+bool	is_delimiter(const char *str);
 
 /*****************************************************************************\
 *                                Cleaning Functions                           *
 \*****************************************************************************/
 
 void	ft_clean_shell(t_shell *shell, char *msg);
+void	ft_clean_cmd_lst(t_cmd *lst, char *msg);
 void	ft_clean_env(t_env *env, char *msg);
+void	ft_clean_tokens(t_token **lst, char *msg);
 void	ft_clean_redirs(t_redir *redirs);
 void	ft_free_args(char **args);
 
---
+++ includes/structs.h
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 17:29:06 by adpinhei          #+#    #+#             */
+/*   Updated: 2025/11/12 19:28:23 by adpinhei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER
+# define PARSER
+
+# include "minishell.h"
+
+/// @brief macros for token identification
+typedef	enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_RED_IN,
+	TOKEN_RED_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_EOF
+}	e_token_type;
+
+typedef	enum e_redir_type
+{
+	REDIR_IN, // <
+	REDIR_OUT, // >
+	REDIR_APPEND, // >>
+	REDIR_HERE // <<
+}	e_redir_type;
+
+/// @brief structs for tolkens
+typedef	struct s_token
+{
+	e_token_type	type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
+
+/// @brief these structs will form our environment.
+typedef	struct s_env
+{
+	char			*name; //variable's name.
+	char			*value; //variable's expansion.
+	struct s_env	*next;
+}	t_env;
+
+/// @brief redirection details. t_cmd will have a pointer to this.
+typedef	struct s_redir
+{
+	e_redir_type	type; //macros for the type of redirection (e.g. '<', '>>')
+	char			*file; //the name of the redirection file. If HEREDOC, this means EOF.
+	int				fd; //holds the redirection file's fd. Will be initialized to -1
+	struct s_redir	*next; //list of redirections (e.g. "< infile cat > outfile", which has both REDIR_IN and REDIR_OUT)
+}	t_redir;
+
+/// @brief main struct to be passed for execution
+typedef	struct s_cmd
+{
+	char			*cmd; //command name
+	char			**args; //arguments to the command
+	t_redir			*redirs; //linked list of redirections, NULL if none
+	struct s_cmd	*next; //if there is a pipe, the address of the next cmd will be here. If no, this will be NULL
+}	t_cmd;
+
+typedef	struct s_shell
+{
+	t_env	*env; //envp list
+	t_token	*tokens; //token list
+	t_cmd	*cmds; //command list
+	int		exit_status; //last command exit status for $?
+}	t_shell;
+
+#endif
