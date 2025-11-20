844477cdef01c1fe5898a7a85d44a38816f0ebcf
AdnilsonPinheiro <adpmaia@gmail.com>
2025-11-20 17:19:15 +0000

    fix(syntax): checking if input ends in a TOKEN_WORD

--- parsing/syntax.c
+++ parsing/syntax.c
@@ -6,7 +6,7 @@
 /*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 20:40:24 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/11/20 15:44:19 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/11/20 17:02:47 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,20 @@ void	ft_syntax(t_shell *shell)
 	tk = shell->tokens;
 	if (tk->type != 0 && tk->type != 2 && tk->type != 5)
 	{
-		ft_clean_shell(shell, "Invalid syntax\n");
+		ft_clean_tokens(&shell->tokens, "Invalid syntax\n");
 		return ;
 	}
 	while (tk)
 	{
-		if (tk->type > 0 && tk->type < 6)
+		if (tk->type > TOKEN_WORD)
 		{
 			if (!tk->next || tk->next->type != TOKEN_WORD)
-			{
-				ft_clean_tokens(&shell->tokens, "Invalid syntax\n");
-				return ;
-			}
+				return (ft_clean_tokens(&shell->tokens, "Invalid syntax\n"));
+		}
+		if (!tk->next && tk->type != TOKEN_WORD)
+		{
+			ft_clean_tokens(&shell->tokens, "Invalid syntax\n");
+			return ;
 		}
 		tk = tk->next;
 	}
