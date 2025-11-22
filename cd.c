c531b978aa1dbdbb65c03b8378c74e869bebe553
Bruna Maria Duarte Barbosa <brmaria-@c1r16s1.42porto.com>
2025-11-22 19:46:16 +0000

    finishing cd

--- cd.c
+++ cd.c
@@ -6,8 +6,30 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 16:09:43 by brmaria-          #+#    #+#             */
-/*   Updated: 2025/11/20 16:09:53 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/11/22 19:12:53 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_cd()
+#include "builtins.h"
+
+
+
+void	ft_cd(char **args, t_env **env)
+{
+	char	old_pwd;
+	char	pwd;
+	char	target_pwd;
+	int		result;
+	
+	if (!args)
+		return (1);
+	old_pwd = get_from_env("OLDPWD", env);
+	pwd = get_from_env("PATH", env);
+	result = chdir(target_pwd);
+	if (result == -1)
+	{
+		//error message
+		//free o que for necessario
+		return (1);
+	}
+}
--- pwd.c
+++ pwd.c
@@ -6,7 +6,7 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 16:31:29 by brmaria-          #+#    #+#             */
-/*   Updated: 2025/11/15 17:14:29 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/11/22 19:27:55 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,23 @@
 /// @brief Gets PWD's content from env
 /// @param env env's list
 /// @return PWD's content
+
+char	*get_current_path(void)
+{
+	char	*cwd;
+	int	size;
+
+	size = 1024;
+	cwd = malloc(size);
+	if (!cwd)
+	{
+		perror("cd");
+		return (1);
+	}
+	getcwd(cwd, )
+	return (cwd);
+}
+
 char *getpwd(t_env *env)
 {
 	while(env)
