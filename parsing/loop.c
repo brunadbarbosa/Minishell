064cc612fcd8bf6366283a2e293a1f9e583c3559
AdnilsonPinheiro <adpmaia@gmail.com>
2025-11-24 15:10:59 +0000

    initial token can now be anything other than TOKEN_PIPE

--- parsing/loop.c
+++ parsing/loop.c
@@ -6,7 +6,7 @@
 /*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:36:36 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/11/21 16:21:32 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/11/24 15:10:10 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	loop(t_shell *shell)
 		add_history(input);
 		ft_lexer(shell, input);
 		ft_syntax(shell);
-		ft_printlst(shell);
+//		ft_printlst(shell);
 		ft_parser(shell);
 //		ft_printcmd(shell);
 		free(input);
