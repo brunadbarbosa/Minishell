5d730a8385d1050ecbc97a94d823970852a5a738
Bruna Maria Duarte Barbosa <brmaria-@c1r13s3.42porto.com>
2025-12-01 16:42:02 +0000

    some organizations at Makefile

--- parsing/loop.c
+++ parsing/loop.c
@@ -6,7 +6,7 @@
 /*   By: brmaria- <brmaria-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:36:36 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/11/30 18:15:02 by brmaria-         ###   ########.fr       */
+/*   Updated: 2025/12/01 16:32:29 by brmaria-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ void	loop(t_shell *shell)
 		ft_syntax(shell);
 		// ft_printlst(shell);
 		ft_parser(shell);
+		
 //		ft_printcmd(shell);
 		free(input);
 		ft_clean_tokens(&shell->tokens, NULL);
