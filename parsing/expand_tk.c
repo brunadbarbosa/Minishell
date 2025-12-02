97fdfbd62552707d482dd4321b09bb026d52d0db
AdnilsonPinheiro <adpmaia@gmail.com>
2025-12-02 19:03:09 +0000

    expansions working properly. Must refactor norm

--- parsing/expand_tk.c
+++ parsing/expand_tk.c
@@ -6,13 +6,16 @@
 /*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 14:19:29 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/12/01 20:40:50 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/12/02 18:58:57 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void static	ft_removequotes(char **str, char *set);
+static void	ft_removequotes(char **str, char *set);
+static char	*ft_replace(char *old, t_env *env);
+static char	*ft_newstr(char *old, t_env *env);
+static char	*ft_findexp(char *var_start, int *var_len, t_env *env);
 
 /// @brief expands the tokens when necessary
 /// @param lst token list
@@ -31,14 +34,12 @@ void	ft_expand(t_token **lst, t_env *env)
 		{
 			if (current->value[0] == '\'')
 				ft_removequotes(&current->value, "\'");
-			else if (current->value[0] == '\"')
+			else 
 			{
-				ft_removequotes(&current->value, "\"");
-				ft_replace(current->value, &expanded, env);
-			}
-			else
-			{
-				ft_replace(&current->value, &expanded, env);
+				if (current->value[0] == '\"')
+					ft_removequotes(&current->value, "\"");
+				expanded = ft_replace(current->value, env);
+				free(current->value);
 				current->value = expanded;
 			}
 		}
@@ -46,10 +47,7 @@ void	ft_expand(t_token **lst, t_env *env)
 	}
 }
 
-/// @brief trims quotes from token->value
-/// @param str the string to be trimmed
-/// @param set the quotes to be removed
-void static	ft_removequotes(char **str, char *set)
+static void	ft_removequotes(char **str, char *set)
 {
 	char	*trimmed;
 
@@ -60,9 +58,97 @@ void static	ft_removequotes(char **str, char *set)
 	*str = trimmed;
 }
 
-static void	ft_replace(char **old, char **new, t_env *env)
+static char	*ft_replace(char *old, t_env *env)
 {
-	if (!env || !old || !*old)
-		return ;
-	
+	char	*new;
+
+	if (!old || !env)
+		return (NULL);
+	new = ft_newstr(old, env);
+	return (new);
+}
+
+static char	*ft_newstr(char *old, t_env *env)
+{
+	char	*new;
+	char	*expansion;
+	int		var_len;
+	size_t	new_size;
+	int		i;
+	int		j;
+
+	if (!old || !env)
+		return (NULL);
+	var_len = 0;
+	new_size = 0;
+	i = 0;
+	j = 0;
+	new = NULL;
+	while (old[i])
+	{
+		if (old[i] == '$' && old[i + 1] && (old[i + 1] == '_' || ft_isalnum(old[i + 1])))
+		{
+			expansion = ft_findexp(&old[i + 1], &var_len, env);
+			new_size += ft_strlen(expansion);
+			free(expansion);
+			i += var_len + 1;
+		}
+		else
+		{
+			new_size++;
+			i++;
+		}
+	}
+	new = malloc(sizeof(char) * (new_size + 1));
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (old[i])
+	{
+		if (old[i] == '$' && old[i + 1] && (old[i + 1] == '_' || ft_isalnum(old[i + 1])))
+		{
+			expansion = ft_findexp(&old[i + 1], &var_len, env);
+			if (expansion)
+			{
+				ft_memmove(&new[j], expansion, ft_strlen(expansion));
+				j += ft_strlen(expansion);
+				free(expansion);
+			}
+			i += var_len + 1;
+		}
+		else
+		{
+			new[j] = old[i];
+			j++;
+			i++;
+		}
+	}
+	new[j] = '\0';
+	return (new);
+}
+
+static char	*ft_findexp(char *var_start, int *var_len, t_env *env)
+{
+	t_env	*node;
+	int		i;
+
+	if (!var_start || !env)
+		return (ft_strdup(""));
+	*var_len = 0;
+	i = 0;
+	while (var_start[*var_len] && (ft_isalnum(var_start[*var_len]) || var_start[*var_len] == '_'))
+		(*var_len)++;
+	if (*var_len == 0)
+		return (ft_strdup(""));
+	node = env;
+	while (node)
+	{
+		i = 0;
+		while (i < *var_len && var_start[i] && node->name[i] && var_start[i] == node->name[i])
+			i++;
+		if (i == *var_len && node->name[i] == '\0')
+			return (ft_strdup(node->value));
+		node = node->next;
+	}
+	return (ft_strdup(""));
 }
--- parsing/loop.c
+++ parsing/loop.c
@@ -6,7 +6,7 @@
 /*   By: adpinhei <adpinhei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:36:36 by adpinhei          #+#    #+#             */
-/*   Updated: 2025/12/01 18:01:58 by adpinhei         ###   ########.fr       */
+/*   Updated: 2025/12/02 19:01:07 by adpinhei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ void	loop(t_shell *shell)
 	while (true)
 	{
 		input = readline("minishell> ");
-		ft_exit(shell, input); //that goes out
+		ft_exit(shell, input); //that goes out. Replace this for a simple quote syntax checker
 		add_history(input);
 		ft_lexer(shell, input);
 		ft_syntax(shell);
 //		ft_printlst(shell);
 		ft_parser(shell);
-		ft_printcmd(shell);
+//		ft_printcmd(shell);
 		free(input);
 		ft_clean_tokens(&shell->tokens, NULL);
 		ft_openredirs(shell->cmds);
